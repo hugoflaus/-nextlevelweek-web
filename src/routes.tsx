5140cdc5e6e53e7bf9230c52524bb79bc7e6e783
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-04 20:05:40 -0300

    Create file routes

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+import CreatePotin from './pages/CreatePoint';
+
+const Routes = () => {
+  return(
+    <BrowserRouter>
+      <Route  component={ Home } path="/" exact />
+      <Route  component={ CreatePotin } path="/create-point" />
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
