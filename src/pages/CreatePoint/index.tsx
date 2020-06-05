3655fb658b4eb37c59ec4ae12b7fdd57a926369f
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-05 07:01:49 -0300

    Implement register point

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useEffect, useState, ChangeEvent, FormEvent} from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -31,14 +31,22 @@ const CreatePotin = () =>{
 
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
 
+  const [formData, setFomrData] = useState({
+    name: '',
+    email: '',
+    whatsapp: '',
+  });
+
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+  const [selectedItems, setSelectedItems] =  useState<number[]>([]);
   
+
+  const history = useHistory();
 
   useEffect(() =>{
    navigator.geolocation.getCurrentPosition(position => {
-     console.log(position)
    const {latitude, longitude } = position.coords;
 
    setInitialPosition([latitude, longitude]);
@@ -89,6 +97,50 @@ const CreatePotin = () =>{
     ]);
   }
 
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+    const { name, value } = event.target;
+    setFomrData({...formData, [name]: value});
+  }
+  
+  function handleSelectItem(id: number){
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if(alreadySelected >= 0){
+      const filteredItems = selectedItems.filter(item => item != id);
+      setSelectedItems(filteredItems);
+    }else{
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  async function handleSubmit(event: FormEvent){
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    };
+
+    await api.post('points', data);
+
+    alert('Ponto de coleta criado!');
+
+    history.push('/');
+
+  }
+
   return(
     <div id="page-create-point">
       <header>
@@ -99,7 +151,7 @@ const CreatePotin = () =>{
           Voltar para home
         </Link>
       </header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
 
         <fieldset>
@@ -107,30 +159,33 @@ const CreatePotin = () =>{
             <h2>Dados</h2>
           </legend>
           <div className="field">
-            <label htmlFor="email">Email</label>
+          <label htmlFor="name">Nome da entidade</label>
             <input 
-              type="email"
-              name="email"
-              id="email"
+              type="text"
+              name="name"
+              id="name"
+              onChange={handleInputChange}
             />
           </div>
           <div className="field-group">
+            <div className="field">
+            <label htmlFor="email">Email</label>
+              <input 
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleInputChange}
+              />
+            </div>
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
               <input 
                 type="text"
                 name="whatsapp"
                 id="whatsapp"
+                onChange={handleInputChange}
               />
             </div>
-          <div className="field">
-            <label htmlFor="name">Nome da entidade</label>
-            <input 
-              type="text"
-              name="name"
-              id="name"
-            />
-          </div>
           </div>
         </fieldset>
         <fieldset>
@@ -184,7 +239,10 @@ const CreatePotin = () =>{
           </legend>
           <ul className="items-grid">
             {items.map(item => (
-              <li key={item.id}>
+              <li key={item.id} 
+                onClick={() => handleSelectItem(item.id)}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                >
                 <img src={item.image_url} alt={item.title}/>
                 <span>{item.title}</span>
               </li>
